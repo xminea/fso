659b535638672e9912e618f7dac1e746a4b8c3ac
Minea Ervio <minea.ervio@aalto.fi>
2020-05-07 18:12:12 +0300

    tehtävä 1.5, 1.4:stä muokattu

--- Osio1/kurssitiedot/src/index.js
+++ Osio1/kurssitiedot/src/index.js
@@ -2,24 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
   };
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1.exercises} part2={part2.exercises} part3={part3.exercises} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
@@ -35,9 +39,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   );
 };
@@ -55,7 +59,10 @@ const Part = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.part1 + props.part2 + props.part3}</p>
+      <p>
+        Number of exercises{' '}
+        {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+      </p>
     </div>
   );
 };
