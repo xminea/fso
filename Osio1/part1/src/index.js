6749fffa25c65dc1c4e4c6c883f978ce24d59555
Minea Ervio <minea.ervio@aalto.fi>
2020-05-08 01:20:20 +0300

    testiohjelman muokkailua ja unicafe, t 1.6

--- Osio1/part1/src/index.js
+++ Osio1/part1/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+/* eslint-disable */
+
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
+/* const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  };
   return (
     <div>
       <p>
         Hello {props.name}, you are {props.age} years old
       </p>
+      <p>So you were probably boen {bornYear()}</p>
     </div>
   );
 };
@@ -20,6 +27,25 @@ const App = () => {
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={nimi} age={ika} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+*/
+
+const App = (props) => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+
+  return (
+    <div>
+      <div>
+        {left}
+        <button onClick={() => setLeft(left + 1)}>left</button>
+        <button onClick={() => setRight(right + 1)}>right</button>
+        {right}
+      </div>
     </div>
   );
 };
