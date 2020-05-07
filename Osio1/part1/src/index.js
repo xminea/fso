dfd774273175c518135f504049d86fefd321386a
Minea Ervio <minea.ervio@aalto.fi>
2020-05-07 13:13:21 +0300

    tehtävä 1.1 ja testiohjelma part1

---
+++ Osio1/part1/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const nimi = 'Pekka';
+  const ika = 10;
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
