972d7a5fff0685f511cb729701fcf88064d994a1
Minea Ervio <minea.ervio@aalto.fi>
2020-12-30 12:28:09 +0200

    5.13

--- Osio5/bloglist-frontend/src/components/Blog.test.js
+++ Osio5/bloglist-frontend/src/components/Blog.test.js
@@ -1,24 +1,38 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 //<Blog key={blog.id} blog={blog} username={user.name} likeBlog={() => likeBlog(blog.id)} removeBlog={() => removeBlog(blog)}/>
 
-test('renders title and author, not url and likes', () => {
-  const blog = {
-    title: 'Sääennuste',
-    author: 'Pekka Pouta',
-    likes: 69,
-    url: 'pekkanet.com'
-  }
+describe('Element Blog', () => {
+    const blog = {
+        title: 'Sääennuste',
+        author: 'Pekka Pouta',
+        likes: 69,
+        url: 'pekkanet.com'
+      }
+    let component
+    beforeEach(() => {
+        component = render(
+            <Blog blog={blog} />
+        )
+    })
+      
+    test('renders title and author, not url and likes', () => {
+        
+      
+        expect(component.container).toHaveTextContent(blog.title + ' ' + blog.author)
+        expect(component.queryByText(blog.url)).toBeNull()
+        expect(component.queryByText(blog.likes.toString())).toBeNull()
+    })
+      
+    test('renders url and likes when opened', () => {
+        const viewButton = component.getByText('view')
 
-  const component = render(
-    <Blog blog={blog} />
-  )
-
-  expect(component.container).toHaveTextContent(
-    'Sääennuste Pekka Pouta'
-  )
-  expect(component.queryByText(blog.url)).toBeNull()
-  expect(component.queryByText(blog.likes.toString())).toBeNull()
+        fireEvent.click(viewButton)
+        expect(component.container).toHaveTextContent(blog.title)
+        expect(component.container).toHaveTextContent(blog.url)
+        expect(component.container).toHaveTextContent(blog.likes)
+        expect(component.container).toHaveTextContent(blog.author)
+    })
 })
